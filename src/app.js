d2b019100220dbeaf1b48b71151200633ccbc7e1
Laxus66 <Laxus2301@gmail.com>
2023-06-02 22:40:38 +0700

    [G2W] Khung Dữ Liệu

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
+
+app.use(express.json())
+
+//routes
+
+
+mongoose.connect("")
+export const viteNodeApp = app
